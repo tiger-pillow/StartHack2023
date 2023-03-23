0bc3c6f93b6980212e3026798f0ab75c9a1131d7
yiyuan <gladysfangcs@gmail.com>
2023-03-23 01:43:38 +0100

    added tailwindcss, started the index page

--- starthack/src/App.js
+++ starthack/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Cecylia Denis Vincent Yiyuan <br></br>
-        Start Hack case solution for Supercell 
+        <header className='mt-10 text-3xl text-center font-bold underline'>
+          Group vycd solution for supercell </header>
+        <p className='text-center font-sm'>Cecylia Denis Vincent Yiyuan </p>
+        <div className='ml-40 mr-40  border-4 border-black flex h-40'> 
+          <p className="m-5 text-center">A video of Clash of Clans will be here</p>
+        </div>
+        <button> Training </button>
+        
       </header>
     </div>
   );
