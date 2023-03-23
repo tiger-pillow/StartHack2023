23baa86bf770b5f8f59477b87c012b2d0bc5513d
yiyuan <gladysfangcs@gmail.com>
2023-03-23 01:20:30 +0100

    added react packages

---
+++ starthack/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Cecylia Denis Vincent Yiyuan <br></br>
+        Start Hack case solution for Supercell 
+      </header>
+    </div>
+  );
+}
+
+export default App;
